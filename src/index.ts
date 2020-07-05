097ea978696c315fe6fe8fe7c93e62e337ad3d30
Andrew1407 <endry1407@gmail.com>
2020-07-06 02:09:31 +0300

    add modules

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import DichBoxDB from './database/DichBoxDB';
 import * as express from 'express';
 import { Application, Request, Response } from 'express';
-
+import * as cors from 'cors';
+import * as bodyparser from 'body-parser';
 
 const app: Application = express();
+app.use(cors());
+app.use(bodyparser.json())
 
 app.get('/', (req: Request, res: Response) => {
-  res.json({ a: 'dich-server'});
-  res.end()
+  res.sendFile('./view/index.html', { root: __dirname });
 });
 
 app.listen(7041, '192.168.0.223')
