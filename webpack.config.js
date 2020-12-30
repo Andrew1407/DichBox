d3eeed29d967da9dbf7a1ac8d1ab2caafaeb08ce
dgomilko <digomilko@gmail.com>
2020-12-30 16:42:04 +0200

    server: webpack environment

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const fs = require('fs');
+const nodeExternals = require('webpack-node-externals')
+
+module.exports = {
+  mode: "production",
+  entry: path.resolve(__dirname, './src/index.ts'),
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'server.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  target: 'node',
+  externals: [nodeExternals({})]
+};
