18ac0767665881168b3628a944abb15b0cc23e4a
Andrew1407 <endry1407@gmail.com>
2021-01-01 18:46:52 +0200

    server: removing conf. files

--- src/controllers/boxes/boxesMiddleware.ts
+++
@@ -1,6 +0,0 @@
-import boxesController from './boxesController';
-import { getMiddlewares } from '../extra';
-import { boxesRouters } from '../routesTypes';
-
-const boxesMiddlewares: boxesRouters = getMiddlewares(boxesController);
-export default boxesMiddlewares;
--- src/controllers/user/userMiddleware.ts
+++
@@ -1,6 +0,0 @@
-import userController from './userController';
-import { getMiddlewares } from '../extra';
-import { userRouters } from '../routesTypes';
-
-const userMiddlewares: userRouters = getMiddlewares(userController);
-export default userMiddlewares;
