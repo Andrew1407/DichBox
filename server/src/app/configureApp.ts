2438c801e03f74d544edb9c6d070188d8a0b25c5
Andrew1407 <endry1407@gmail.com>
2021-05-20 16:25:13 +0300

    server: deprecated bodyparser removed

--- server/src/app/configureApp.ts
+++ server/src/app/configureApp.ts
@@ -1,5 +1,4 @@
 import * as express from 'express';
-import * as bodyparser from 'body-parser';
 import * as cors from 'cors';
 import usersRouter from '../routes/userRoutes';
 import boxesRouter from '../routes/boxesRoutes';
@@ -9,8 +8,8 @@ const configureApp = (app: express.Application): void => {
   const limit: string = '100mb';
   app
     .use(cors())
-    .use(bodyparser.json({ limit }))
-    .use(bodyparser.urlencoded({ limit, extended: true }))
+    .use(express.json({ limit }))
+    .use(express.urlencoded({ limit, extended: true }))
     .use(express.static(viewPath))
     .use('/users', usersRouter)
     .use('/boxes', boxesRouter)
